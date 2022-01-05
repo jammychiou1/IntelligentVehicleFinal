fc109084da248b09c0ec144fa4c62abe65d8373b
jammychiou1 <jammy.chiou1@gmail.com>
2022-01-06 04:10:57 +0800

    intersection constructor skeleton done

--- intersection_controller.cpp
+++ intersection_controller.cpp
@@ -4,11 +4,57 @@
 
 #include "vehicle.h"
 
-IntersectionController::IntersectionController(int id, int *time_p) {
+using std::pair, std::vector, std::map;
+using std::make_pair;
+
+IntersectionController::IntersectionController(int id, vector<int> conflict_zone_ids, map<int, vector<TrajectoryDesc>> in_lane_to_trajectories, int *time_p) {
     //assert(time_p != nullptr);
     _time_p = time_p;
+    for (int czid : conflict_zone_ids) {
+        _conflict_zones[czid] = {};
+    }
+    for (auto &p : in_lane_to_trajectories) {
+        int in_lane_id = p.first;
+        vector<TrajectoryDesc> &trajs = p.second;
+
+        _InLane in_lane;
+        in_lane.in_lane_id = in_lane_id;
+        in_lane.queued_vehicles = {};
+        in_lane.trajectories = {};
+        //map<int, _Trajectory> out_lanes_to_traj;
+        for (TrajectoryDesc &traj : trajs) {
+            for (int czid : traj.conflict_zone_ids) {
+                assert(_conflict_zones.count(czid));
+                _conflict_zones[czid].trajectories_over.push_back(make_pair(in_lane_id, traj.out_lane_id));
+            }
+
+            _Trajectory new_traj;
+            //new_traj.in_lane_id = traj.in_lane_id;
+            //new_traj.out_lane_id = traj.out_lane_id;
+            new_traj.conflict_zone_ids = traj.conflict_zone_ids;
+            assert(!in_lane.trajectories.count(traj.out_lane_id));
+            in_lane.trajectories[traj.out_lane_id] = new_traj;
+        }
+
+        assert(!_in_lanes.count(in_lane_id));
+        _in_lanes[in_lane_id] = in_lane;
+    }
 }
+
 void IntersectionController::enter(Vehicle *veh_p, int in_lane_id, int out_lane_id) {
+    assert(_in_lanes.count(in_lane_id));
+    _InLane &in_lane = _in_lanes[in_lane_id];
+    in_lane.queued_vehicles.push_back(veh_p);
+
+    assert(in_lane.trajectories.count(out_lane_id));
+    //_Trajectory &traj = in_lane.trajectories[out_lane_id];
+
+    assert(!_vehicle_states.count(veh_p));
+    _VehState state;
+    state.in_lane_id = in_lane_id;
+    state.out_lane_id = out_lane_id;
+    _vehicle_states[veh_p] = state;
 }
+
 void update() {
 }
--- intersection_controller.h
+++ intersection_controller.h
@@ -2,22 +2,48 @@
 #define INTERSECTION_CONTROLLER_H
 
 #include <vector>
+#include <queue>
+#include <utility>
 
 #include "graph.h"
 
 class Vehicle;
 
+struct TrajectoryDesc {
+    //int in_lane_id;
+    int out_lane_id;
+    std::vector<int> conflict_zone_ids;
+};
+
 class IntersectionController {
     public:
-        IntersectionController(int id = 0, int *time_p = nullptr);
+        IntersectionController(int id = 0, std::vector<int> conflict_zone_ids = {}, std::map<int, std::vector<TrajectoryDesc>> in_lane_to_trajectories = {}, int *time_p = nullptr);
 		void enter(Vehicle *veh_p, int in_lane_id, int out_lane_id);
 		void update();
     private:
         struct _VehState {
             //Vehicle *veh_p;
+            int in_lane_id;
+            int out_lane_id;
+        };
+        struct _Trajectory {
+            //int in_lane_id;
+            //int out_lane_id;
+            std::vector<int> conflict_zone_ids;
+            std::deque<Vehicle*> moving_vehicles;
+        };
+        struct _InLane {
+            int in_lane_id;
+            std::deque<Vehicle*> queued_vehicles;
+            std::map<int, _Trajectory> trajectories;
+        };
+        struct _ConflictZone {
+            std::vector<std::pair<int, int>> trajectories_over;
         };
         Graph _graph;
         std::map<Vehicle*, _VehState> _vehicle_states;
+        std::map<int, _InLane> _in_lanes;
+        std::map<int, _ConflictZone> _conflict_zones;
         int *_time_p;
 };
 
